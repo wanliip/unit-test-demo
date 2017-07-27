bbc35750c329816480957f2bfa0dbafcbc861d8d
yanglei <yanglei@xingshulin.com>
2017-07-27 17:31:54 +0800

    [杨磊]删除无用文件

--- target/classes/db-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE T_LANGUAGE (
-  id INT NOT NULL AUTO_INCREMENT,
-  name varchar(256) ,
-  description varchar(256) DEFAULT '',
-  create_time datetime NOT NULL as CURRENT_TIMESTAMP ,
-  update_time datetime NOT NULL as CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-);
--- target/classes/db-test-data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `t_language` (`id`, `name`, `description`, `create_time`, `update_time`)
-VALUES
-	(1, 'JAVA', 'Java是一种广泛使用的计算机编程语言，拥有跨平台、面向对象、泛型编程的特性.', '2017-07-27 15:07:05', '2017-07-27 15:07:32');
-INSERT INTO `t_language` (`id`, `name`, `description`, `create_time`, `update_time`)
-VALUES
-	(2, 'C', 'C是一种通用的程式語言，广泛用于系统软件与应用软件的开发.', '2017-07-27 15:07:37', '2017-07-27 15:07:45');
